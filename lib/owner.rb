195b930677d5c05252bf6d2df2b81819368af18e
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-09-20 18:56:43 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -55,7 +55,7 @@ class Owner
    pets = self.dogs + self.cats
    
    pets.each {|pet| pet.mood = "nervous"}
-   pets.each {|pet| pet.owner = "nil"}
+   pets.each {|pet| pet.owner = nil}
    
  end
 end
