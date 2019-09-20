d840ac1ec7f43a687b8f25d5f452ffc803a0565f
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-09-20 18:46:39 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,49 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+  
+ def initialize(name, species = "human")
+   @name = name
+   @species = species
+   @@all << self
+ end
+ 
+ def say_species
+   "I am a #{species}."
+ end
+ 
+ def self.all
+   @@all
+ end
+ 
+ def self.count
+   self.all.count
+ end
+ 
+ def self.reset_all
+   self.all.clear
+ end
+ 
+ def cats
+   Cat.all.select {|cat| cat.owner == self}
+ end
+ 
+ def dogs 
+   Dog.all.select {|dog| dog.owner == self}
+ end
+ 
+ def buy_cat(name)
+   Cat.new(name, self)
+   #make new instance of the pet
+   #initialize it with that name and the owner who is purchasing it
+ end
+ 
+ def buy_dog(name)
+   Dog.new(name, self)
+ end
+ 
+ def walk_dogs
+   dog.mood = "happy"
+ end
 end
